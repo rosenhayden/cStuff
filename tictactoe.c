0c733dfb0d98609858148428bb159e6c8fe8f3ae
Hayden Rosen <91492781+rosenhayden@users.noreply.github.com>
2024-01-29 11:50:32 -0800

    Add files via upload

--- tictactoe.c
+++ tictactoe.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//Lets try to make the game board as chars and append the selected slot on the board to either x if player b or y if player a
 int gameBoard[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 int playerA[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 int playerB[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
@@ -12,6 +12,7 @@ int roundCount = 0;
 
 
 void checkWinner(int player[][3], int player2[][3], int rows) { 
+  if(roundCount >=3) {
   int tempX = 0;
   int tempY = 0;
   
@@ -26,12 +27,22 @@ void checkWinner(int player[][3], int player2[][3], int rows) {
     printf("Player A WINS\n");
     gameOver = 0;  
   }
+  tempX = 0;
+  tempY = 0;
+  for(int i = 0; i < rows; i++) {
+    tempX += playerB[i][0]; 
+      }
+
+  for(int j = 0; j < rows; j++) {
+      tempY += playerB[0][j];
+    }
 if(tempX == 3 || tempY == 3 || playerB[0][0] == 1 && playerB[1][1] == 1 && playerB[2][2] || playerB[2][0] == 1 && playerB[1][1] == 1 && playerB[0][2] == 1) {
-    printf("Player A WINS\n");
+      printf("\e[1;1H\e[2J");
+      printf("Player B WINS\n");
     gameOver = 0;  
   }
 }
-
+}
 
 void initializeGameBoard(){
   printf("Game Board:\n");
@@ -46,6 +57,8 @@ void initializeGameBoard(){
 
 void gameLoop() {
 while(gameOver == 1) { 
+  
+  printf("\e[1;1H\e[2J");
   printf("Player A choose x\n");
   scanf("%d", &playerInputX);
   printf("choose y\n", &playerInputY);
@@ -79,17 +92,19 @@ while(gameOver == 1) {
   }
 
   checkWinner(playerA, playerB, 3);
+  roundCount++;
 
+  printf("Current round: %d\n", roundCount);
+  if (roundCount > 9) {
+      gameOver = 0;
+    }
 }
 
 }
+
 int main() {
-
-initializeGameBoard();
-gameLoop();
-
- 
-
+  initializeGameBoard();
+  gameLoop();
   return 0;
 }
 
